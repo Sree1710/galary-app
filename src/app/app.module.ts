66eca7de73782ae3df70781587e0ec21c4eb200e
Sree1710 <kunjusree817@gmail.com>
2022-07-08 12:45:49 +0530

    galary-app fully created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewGalaryComponent } from './view-galary/view-galary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewGalaryComponent
   ],
   imports: [
     BrowserModule,
